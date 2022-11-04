8be9609f1258e530fc8577a7edf7d3e431f7533e
technaveenks <tech.naveenks@gmail.com>
2022-11-04 16:05:04 +0530

    Initial Commit

---
+++ src/main/java/com/sira/learning/SelAutomation/App.java
@@ -0,0 +1,13 @@
+package com.sira.learning.SelAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
